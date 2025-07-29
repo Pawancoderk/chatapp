764c8478d376378a13d3868a8413508952371882
Pawan <imrealpawan@gmail.com>
2025-07-29 21:15:01 +0530

    feat(chat): implement getAllChats controller with user data fetching and unseen messages count

--- backend/chat/src/controllers/Chat.ts
+++ backend/chat/src/controllers/Chat.ts
@@ -1,5 +1,10 @@
 import TryCatch from "../config/TryCatch.js";
 import { AuthenticatedRequest } from "../middlewares/isAuth.js";
+import {Chat} from "../models/Chat.js"
+import Messages from "../models/Messages.js"
+import axios from "axios";
+
+
 
 export const createNewChat = TryCatch(async(req:AuthenticatedRequest,res)=>{
     const userId = req.user?._id;
@@ -11,6 +16,93 @@ export const createNewChat = TryCatch(async(req:AuthenticatedRequest,res)=>{
         })  
         return;
     }
+    const existingChat = await Chat.findOne({
+        users: {$all:[userId,otherUserId], $size:2},
 
-    
+    })
+
+    if(existingChat){
+      res.json({
+        message:"Chat already exists",
+        chatId:existingChat._id
+      })
+      return;
+    }
+    const newChat = await Chat.create({
+        users: [userId, otherUserId]
+    })
+
+    res.status(201).json({
+        message: "Chat created successfully",
+        chatId: newChat._id
+    })
+
+})
+ 
+// ! Learn this route
+export const getAllChats = TryCatch(async(req:AuthenticatedRequest,res)=>{
+    const userId = req.user?._id
+    if(!userId){
+        res.status(400).json({
+            message:"UserId missing",
+            status: false
+        })
+        return;
+    }
+    const chats = await Chat.find({users:userId}).sort({updatedAt: -1})
+
+    const chatWithUserData = await Promise.all(
+        chats.map(async(chat)=>{
+            const otherUserId = chat.users.find((id)=> id !== userId);
+
+            // ! fetching useen messages count
+            const unseenCount = await Messages.countDocuments({
+               chatId: chat._id,
+               seen: false,
+                sender: {$ne: userId}
+            })
+
+            try {
+                const {data} = await axios.get(
+                    `${process.env.USER_SERVICE}/api/v1/user/${otherUserId}`,
+
+                )
+
+                return{
+                    user: data,
+                    chat:{
+                        ...chat.toObject(),
+                        latestMessage:chat.latestMessage || null,
+                        unseenCount,
+                    },
+                };
+
+            } catch (error) {
+                console.error("Error fetching user data:", error)
+                return {
+                    user: {
+                        _id: otherUserId,
+                        name: "Unknown User",
+                    },
+                    chat: {
+                        ...chat.toObject(),
+                        latestMessage: chat.latestMessage || null,
+                        unseenCount,
+                    },
+                };
+            }
+        })
+
+        
+    )
+
+    res.status(200).json({
+        message: "Chats fetched successfully",
+        chats: chatWithUserData,
+    })
+})
+
+export const sendMessage = TryCatch(async(req:AuthenticatedRequest,res)=>{
+     const senderId = req.user?._id
+     const {chatId, text} = req.body
 })
