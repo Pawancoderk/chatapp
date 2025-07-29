87187a63bf446491fae9e6fd660088ef174b4a96
Pawan <imrealpawan@gmail.com>
2025-07-29 21:17:23 +0530

    feat(chat): add chat routes for creating and fetching chats

---
+++ backend/chat/src/routes/chat.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import isAuth from "../middlewares/isAuth.js";
+import { createNewChat, getAllChats } from "../controllers/Chat.js";
+
+
+const router = express.Router()
+router.post("/chat/new",isAuth,createNewChat)
+router.get("/chat/all", isAuth,getAllChats)
+
+
+export default router;
