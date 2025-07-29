dc65274db90ba4c1c967943f60652e481ad043a3
Pawan <imrealpawan@gmail.com>
2025-07-29 21:18:35 +0530

    feat(server): set up express app with JSON parsing and chat routes

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,11 +1,15 @@
 import express, { urlencoded } from "express";
 import dotenv from "dotenv"
 import connectDb from "./config/db.js";
+import chatRoutes from "./routes/chat.js"
 dotenv.config();
 
 connectDb()
 
 const app =  express();
+
+app.use(express.json())
+app.use("/api/v1",chatRoutes)
 app.use(urlencoded({extended:true}));
 
 const Port = process.env.PORT;
