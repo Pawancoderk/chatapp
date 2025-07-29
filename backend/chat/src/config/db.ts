06bbaadebf90a4c8f0bcd1b63d406275af2fddc1
Pawan <imrealpawan@gmail.com>
2025-07-29 12:52:12 +0530

    feat(db): implement MongoDB connection setup with error handling

---
+++ backend/chat/src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const connectDb = async ()=>{
+    const url = process.env.MONGO_URL;
+
+    if(!url){
+        throw new Error("MongoDB connection string is not defined in environment variables.");
+    }
+
+    try {
+       await mongoose.connect(url,{
+            dbName:"chatapp",
+        });
+        console.log("✅Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); 
+    }
+}
+
+export default connectDb;
