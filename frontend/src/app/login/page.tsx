028a0e7398aad5a705a7f641a64f6eb10faea1b9
Pawan <imrealpawan@gmail.com>
2025-08-02 15:53:47 +0530

    feat(login): enhance LoginPage with toast notifications and loading state management

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,29 +1,37 @@
 "use client"
 import { ArrowRight, Loader2, Mail } from 'lucide-react'
-import { useRouter } from 'next/navigation'
+import { redirect, useRouter } from 'next/navigation'
 import {useState} from "react"
 import React from 'react'
 import axios from 'axios'
-import { user_service } from '@/context/Appcontext'
+import { useAppData, user_service } from '@/context/Appcontext'
+import Loading from '@/components/Loading'
+import toast from 'react-hot-toast'
 
 const Loginpage = () => {
     const [email, setEmail] = useState<string>("")
     const [loading, setLoading] = useState<boolean>(false)
-    const router = useRouter()
+    const router = useRouter();
+    
+    const {isAuth, loading: userLoading} = useAppData() 
+
     const handleSubmit = async(e:React.FormEvent<HTMLFormElement>):Promise<void>=>{
         e.preventDefault()
         setLoading(true)
 
         try {
             const {data}  = await axios.post(`${user_service}/api/v1/login`,{email});
-            alert(data.message)
+            toast.success(data.message)
             router.push(`/verify?email=${email}`)
         } catch (error:any) {
-           alert(error.response.data.message)
+           toast.error(error.response?.data?.message)
         }finally{
             setLoading(false)
         }
     }
+
+    if(userLoading) return <Loading/>
+    if(isAuth) return redirect("/chat")
   return (
     <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
         <div className='max-w-md w-full'>
