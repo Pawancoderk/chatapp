fbaccee4e65b3c8971da8f735ccacb4f6086e7a5
Pawan <imrealpawan@gmail.com>
2025-08-02 15:52:38 +0530

    feat(layout): wrap children in AppProvider for context management

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { AppProvider } from "@/context/Appcontext";
 
 
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body
         
       >
-        {children}
+       <AppProvider>
+         {children}
+       </AppProvider>
       </body>
     </html>
   );
